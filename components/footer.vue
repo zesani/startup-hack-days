124a8656f848f512b016ec0bbb9b73ec082aabab
Nati Namvong <nati@flyingcomma.com>
2017-01-15 22:25:35 +0700

    add footer

---
+++ components/footer.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        <p>
+          <a class="icon" target="_blank" href="https://www.facebook.com/theinternshipio/">
+            <i class="fa fa-facebook-square" aria-hidden="true"></i>
+          </a>
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+.icon {
+  color: #000 !important;
+}
+</style>
