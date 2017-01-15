e7c6d69fac3e3152f3ee62abb4fb10c10a278b9c
Nati Namvong <nati@flyingcomma.com>
2017-01-15 21:41:48 +0700

    change og:image

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,9 +9,9 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'theme-color', content: '#29211F' },
       { hid: 'description', content: "The Internship 2017" },
-      { property: 'og:url', content: 'http://www.theinternship.io.s3-website-ap-southeast-1.amazonaws.com' },
+      { property: 'og:url', content: 'http://www.theinternship.io' },
       { property: 'og:title', content: 'The Internship Program ที่สุดของประสบการณ์ฝึกงานกับ Startup ชั้นแนวหน้าของเมืองไทย' },
-      { property: 'og:image', content: 'http://www.theinternship.io.s3-website-ap-southeast-1.amazonaws.com/og.jpg' },
+      { property: 'og:image', content: 'http://www.theinternship.io/og.jpg' },
       { property: 'og:description', content: 'โครงการที่เปิดโอกาสให้นักศึกษา ได้มีโอกาสร่วมงานกับ Tech Startup ชั้นแนวหน้าของไทย กว่าสิบบริษัท โดยจะมีตำแหน่งงานให้เรียนรู้รอบด้าน ไม่ว่าจะเป็น Developer, Tester, Quality Assurance, Graphic Designer, User Experience Design, Product Owner' },
       { property: 'og:type', content: 'website' }
     ],
