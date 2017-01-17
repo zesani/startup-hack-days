6f27f6a0b5f4b6ad0ebed077cfee67f4a8048260
Nati Namvong <nati@flyingcomma.com>
2017-01-17 11:13:36 +0700

    add plugin ga and htojar

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,5 +27,9 @@ module.exports = {
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#3B8070' }
+  loading: { color: '#3B8070' },
+  plugins: [
+    '~plugins/ga.js',
+    '~plugins/hotjar.js'
+  ]
 }
