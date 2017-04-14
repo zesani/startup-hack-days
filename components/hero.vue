9ec703cc14dd6e372d86740cea588f93223b7934
zesani <7sin@outlook.co.th>
2017-04-14 16:12:32 +0700

    update logo bg

--- components/hero.vue
+++ components/hero.vue
@@ -4,10 +4,16 @@
     <!-- Hero content: will be in the middle -->
     <div class="hero-body">
       <div class="container has-text-centered">
-        <img @click="jump()" src="../static/theinternship-logo.png" alt="" class="hero-logo">
+        <div class="columns is-mobile">
+          <div class="column is-12-mobile is-12-tablet">
+            <img @click="jump()" src="../static/hack-logo.png" alt="" class="hero-logo">
+          </div>
+        </div>
         <br><br><br>
         <h1 class="title">
-          โอกาสหนึ่งครั้งของการฝึกงานต้องให้ประสบการณ์ที่ดี<br>ต่อการทำงานตลอดไป
+          อย่ากลัว ถ้าคิดว่าเขียนโปรแกรมไม่เป็น<br>
+ขอแค่มีความเชื่อแล้วก้าวเข้ามา เขียนเป็นแน่นอน
+<!-- ี<br>ต่อการทำงานตลอดไป -->
         </h1>
         <br><br><br>
         <div @click="jump()">
@@ -37,18 +43,20 @@ export default {
 <style lang="css" scoped>
 h1.title {
   font-family: rsu-regular;
+  color: black !important;
 }
 #hero.hero {
+  background-color: #fff;
   /*background-size: cover;*/
   /*background-image: url('/Cover-pic.jpg');*/
-  background: rgba(26,142,250,1);
+  /*background: rgba(26,142,250,1);
   background: -moz-linear-gradient(-45deg, rgba(26,142,250,1) 0%, rgba(89,49,181,1) 100%);
   background: -webkit-gradient(left top, right bottom, color-stop(0%, rgba(26,142,250,1)), color-stop(100%, rgba(89,49,181,1)));
   background: -webkit-linear-gradient(-45deg, rgba(26,142,250,1) 0%, rgba(89,49,181,1) 100%);
   background: -o-linear-gradient(-45deg, rgba(26,142,250,1) 0%, rgba(89,49,181,1) 100%);
   background: -ms-linear-gradient(-45deg, rgba(26,142,250,1) 0%, rgba(89,49,181,1) 100%);
   background: linear-gradient(135deg, rgba(26,142,250,1) 0%, rgba(89,49,181,1) 100%);
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1a8efa', endColorstr='#5931b5', GradientType=1 );
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1a8efa', endColorstr='#5931b5', GradientType=1 );*/
 }
 .hero-head .nav {
   min-height: 100px !important;
@@ -57,7 +65,8 @@ h1.title {
   max-height: 100px;
 }
 .hero-logo {
-  height: 300px;
+  /*height: 300px;*/
+  width: 100%;
 }
 .down {
   font-size: 80px;
